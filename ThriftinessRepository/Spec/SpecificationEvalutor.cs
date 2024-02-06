3b5a4fb4cafa308e5b2159238431f9cb9f40a6b8
Ahmed525-12 <ahmedsayed5250057@gmail.com>
2024-02-06 16:09:03 +0300

    fix errors in specfictions

--- ThriftinessRepository/Spec/SpecificationEvalutor.cs
+++ ThriftinessRepository/Spec/SpecificationEvalutor.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ThriftinessCore.Entites;
+using ThriftinessCore.Specfictions;
 
 namespace ThriftinessRepository.Spec
 {
     public class SpecificationEvalutor<T> where T : BaseEntity
     {
-        public static async Task<IQueryable<T>> GetQuery(IQueryable<T> InputQuery, ISpecifications<T> Spec)
+        public static async Task<IQueryable<T>> GetQuery(IQueryable<T> InputQuery, ISpecfiction<T> Spec)
         {
             var Query = InputQuery;
 
